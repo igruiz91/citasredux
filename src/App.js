467ae9e4857f5c760b16af193a753cea96cd00f6
Israel padigitale <igruiz91@gmail.com>
2019-08-19 17:29:23 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+
+//Redux
+import store from './store';
+import {Provider} from 'react-redux';
+import AgregarCita from './components/AgregarCita';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <Header />
+        <div className="row mt-3">
+          <div className="col-md-6"><AgregarCita /></div>
+          <div className="col-md-6">Listado Aqui</div>
+        </div>
+
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <header>
+            <h1 className="text-center">
+                Administrador de Pacientes de Veterinaria
+            </h1>
+        </header>
+    );
+}
+ 
+export default Header;
